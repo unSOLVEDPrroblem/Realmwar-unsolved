85004df398a0f396c3ad4e984dd16fb5dcae0076
mahanfattahi <mahan1385fattahi@gmail.com>
2025-05-06 16:29:31 +0430

    first changes commited

--- Units.java
+++ Units.java
@@ -1,5 +1,81 @@
 package FinalProject;
 
-public class Units {
+public abstract class Units {
+    protected int health;
+    protected int attackPower;
 
+
+    public Units(int health, int attackPower) {
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+
+    public int getHealth() {
+        return health;
+    }
+
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+
+    public abstract void attack(Units target);
 }
+
+//**  زیر کلاس ها **
+
+class Knight extends Units {
+    public Knight(int health, int attackPower) {
+        super(health, attackPower);
+    }
+
+    @Override
+    public void attack(Units target) {
+    }
+}
+
+class Peasant extends Units {
+    public Peasant(int health, int attackPower) {
+        super(health, attackPower);
+    }
+
+    @Override
+    public void attack(Units target) {
+    }
+}
+
+class Spearman extends Units {
+    public Spearman(int health, int attackPower) {
+        super(health, attackPower);
+    }
+
+    @Override
+    public void attack(Units target) {
+    }
+}
+
+class Swordman extends Units {
+    public Swordman(int health, int attackPower) {
+        super(health, attackPower);
+    }
+
+    @Override
+    public void attack(Units target) {
+    }
+}
+
+
+
+
